3642535226c7b6d532972979b8860dcbbcbccaeb
serdeliverance <sergio.cano@softvision.com>
2019-10-08 14:48:30 -0300

    search todo on controller - first approach

--- src/main/java/com/mavha/mavhachallengetodobespring/service/TodoService.java
+++ src/main/java/com/mavha/mavhachallengetodobespring/service/TodoService.java
@@ -1,10 +1,14 @@
 package com.mavha.mavhachallengetodobespring.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.mavha.mavhachallengetodobespring.domain.StatusEnum;
 import com.mavha.mavhachallengetodobespring.domain.Todo;
 
 public interface TodoService {
 
 	List<Todo> getAll();
+
+	List<Todo> search(Optional<Integer> optId, Optional<String> optDesc, Optional<StatusEnum> map);
 }
--- src/main/java/com/mavha/mavhachallengetodobespring/service/TodoServiceImpl.java
+++ src/main/java/com/mavha/mavhachallengetodobespring/service/TodoServiceImpl.java
@@ -1,10 +1,12 @@
 package com.mavha.mavhachallengetodobespring.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mavha.mavhachallengetodobespring.domain.StatusEnum;
 import com.mavha.mavhachallengetodobespring.domain.Todo;
 import com.mavha.mavhachallengetodobespring.repository.TodoRepository;
 
@@ -17,6 +19,12 @@ public class TodoServiceImpl implements TodoService {
 	@Override
 	public List<Todo> getAll() {
 		return todoRepository.findAll();
+	}
+
+	@Override
+	public List<Todo> search(Optional<Integer> optId, Optional<String> optDesc, Optional<StatusEnum> map) {
+		// TODO implement this method
+		return null;
 	}
 
 }
